545cd0b3a0051713130e26aff6ab7054dedfd3f6
nithin200526 <nandalanithin26@gmail.com>
2025-12-03 09:54:02 +0530

    modified Java program

--- Example.java
+++ Example.java
@@ -1,7 +1,8 @@
 class Example
 {
-public static void main(Strings[] args)
-{
-System.out.println("Welcome to SNB2 Batch");
-}
+	public static void main(Strings[] args)
+	{
+		System.out.println("Welcome to SNB2 Batch");
+		System.out.println("Day 1 : Git Overview");
+	}
 }
